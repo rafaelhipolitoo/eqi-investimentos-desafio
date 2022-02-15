e773ef24dd8d6a43ed3587fdd77ccaedea3dea2d
Rafael Hipólito <contato.rafaelhipolito@gmail.com>
2022-02-15 20:55:24 -0300

    Atualização

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ function moeda(a, e, r, t) {
       , u = tamanho2 = 0
       , l = ajd2 = ""
       , o = window.Event ? t.which : t.keyCode;
-    if (13 == o || 8 == o)
-        return !0;
+      a.value = a.value.replace('R$ ','');            
     if (n = String.fromCharCode(o),
     -1 == "0123456789".indexOf(n))
         return !1;
-    for (u = a.value.length,
+    for (u = a.value.replace('R$ ','').length,
     h = 0; h < u && ("0" == a.value.charAt(h) || a.value.charAt(h) == r); h++)
         ;
     for (l = ""; h < u; h++)
         -1 != "0123456789".indexOf(a.value.charAt(h)) && (l += a.value.charAt(h));
     if (l += n,
     0 == (u = l.length) && (a.value = ""),
-    1 == u && (a.value = "0" + r + "0" + l),
-    2 == u && (a.value = "0" + r + l),
+    1 == u && (a.value = "R$ 0" + r + "0" + l),
+    2 == u && (a.value = "R$ 0" + r + l),
     u > 2) {
         for (ajd2 = "",
         j = 0,
@@ -26,7 +25,7 @@ function moeda(a, e, r, t) {
             j = 0),
             ajd2 += l.charAt(h),
             j++;
-        for (a.value = "",
+        for (a.value = "R$ ",
         tamanho2 = ajd2.length,
         h = tamanho2 - 1; h >= 0; h--)
             a.value += ajd2.charAt(h);
